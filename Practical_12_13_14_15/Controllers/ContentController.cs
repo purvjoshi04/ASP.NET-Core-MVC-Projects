df5a99184d59a7346e5dc134c888924c3fe58248
--replace-all <purvadjoshi@gmail.com>
2023-11-20 18:06:00 +0530

    Lab Practicals and Practice

--- Practical_12_13_14_15/Controllers/ContentController.cs
+++ Practical_12_13_14_15/Controllers/ContentController.cs
@@ -33,10 +33,6 @@ namespace WebApp.Controllers {
             return $"JSON: {product.Name}";
         }
 
-        //[HttpPost]
-        //[Consumes("application/xml")]
-        //public string SaveProductXml(ProductBindingTarget product) {
-        //    return $"XML: {product.Name}";
-        //}
+       
     }
 }
